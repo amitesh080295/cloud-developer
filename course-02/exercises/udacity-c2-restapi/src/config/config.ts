737741b7a1b9d8b050c65689d71c79128d36f64c
Amitesh_Sinha <amitesh_sinha@dell.com>
2020-05-03 18:39:14 +0530

    Updated config.ts to read environment variables

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   dev: {
-    username: "udagramamiteshdev",
-    password: "PostGres#2020",
-    database: "udagramamiteshdev",
-    host: "udagramamiteshdev.cejeaasmtagr.ap-south-1.rds.amazonaws.com",
+    username: process.env.POSTGRESS_USERNAME,
+    password: process.env.POSTGRESS_PASSWORD,
+    database: process.env.POSTGRESS_DATABASE,
+    host: process.env.POSTGRESS_HOST,
     dialect: "postgres",
-    aws_region: "ap-south-1",
-    aws_profile: "default",
-    aws_media_bucket: "udagram-amitesh-dev",
+    aws_region: process.env.AWS_REGION,
+    aws_profile: process.env.AWS_PROFILE,
+    aws_media_bucket: process.env.AWS_MEDIA_BUCKET,
   },
   prod: {
     username: "",
